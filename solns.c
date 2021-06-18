01b12da361143ef5a8e6948d52cb79ff69056ea2
karthik-8369 <84717577+karthik-8369@users.noreply.github.com>
2021-06-18 21:39:20 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -32,12 +32,10 @@ float average(const int arr[],int n)
 }
 
 
-int mode(int arr[],int n)
-{
-	int num,temp_num;
-	int count=0,temp_count=0;
+int mode(int arr[],int n){
+	int num,temp;
+	int c=0,temp_c=0;
 	// sort(arr,n);
-	
 	for(int i=0;i<n-1;i++){
 		for(int j=0;j<n-1;j++){
 			if(arr[j+1]<arr[j]){
@@ -47,31 +45,26 @@ int mode(int arr[],int n)
 			}
 		}
 	}
-	temp_num=arr[0];
-	for(int i=0;i<n;i++)
-	{
-		if(arr[i]==temp_num){
-			temp_count++;
+	temp=arr[0];
+	for(int i=0;i<n;i++){
+		if(arr[i]==temp){
+			temp_c++;
 		}
 		else{
-			if(temp_count>count){
-				count=temp_count;
-				num=temp_num;
+			if(temp_c>c){
+				c=temp_c;
+				num=temp;
 			}
-			temp_num=arr[i];
-			temp_count=1;
+			temp=arr[i];
+			temp_c=1;
 		}
 	}
-	if(temp_count>count)
-	{
-		count=temp_count;
-		num=temp_num;
+	if(temp_c>c){
+		c=temp_c;
+		num=temp;
 	}
 	return num;
 }
-
-
-
 int factors(int num,int arr[])
 {
 	int prime=2,ref=0;
