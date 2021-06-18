36e90cb8051ca198229d0b0f51573dcf6e71f152
karthik-8369 <84717577+karthik-8369@users.noreply.github.com>
2021-06-18 21:01:14 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,42 +1,71 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
-int max(int max[],int a){
-  int i;
-  for(i=1;i<a;i++){
-  max[0]=max[0]>max[i]?(max[0]):(max[i]);
-  }
-  return max[0];}
-int min(int min[],int a){
-  int i;
-  for(i=1;i<a;i++){
-  min[0]=min[0]<min[i]?(min[0]):(min[i]);
-  }
-  return min[0];}
-float average(int avg[],int a){
-  int i,d=0;
-  for(i=0;i<a;i++){
-  d=d+avg[i];
-  }
-  return d/a;}
-int mode(int mode[],int a){
-  int maxValue = 0, max = 0, i, j;
-
-   for (i = 0; i < a; ++i) {
-      int count = 0;
-      
-      for (j = 0; j < a; ++j) {
-         if (mode[j] == mode[i])
-         ++count;
-      }
-      
-      if (count > max) {
-         max = count;
-         maxValue = mode[i];
-      }
-   }
-
-   return maxValue;
+int max(const int arr[],int n){
+	int Max=arr[0];
+	for(int i=0;i<n;i++){
+		if(arr[i]>Max){
+			Max=arr[i];
+		}
+	}
+	return Max;
 }
+
+int min(const int arr[],int n){
+	int Min=arr[0];
+	for(int i=0;i<n;i++){
+		if(arr[i]<Min){
+			Min=arr[i];
+		}
+	}
+	return Min;
+}
+
+float average(const int arr[],int n){
+	float sum=0.0;
+	for(int i=0;i<n;i++){
+		sum+=arr[i];
+	}
+	return sum/(float)n;
+}
+
+
+int mode(int arr[],int n){
+	int num,temp_num;
+	int count=0,temp_count=0;
+	// sort(arr,n);
+	int temp;
+	for(int i=0;i<n-1;i++){
+		for(int j=0;j<n-1;j++){
+			if(arr[j+1]<arr[j]){
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+	temp_num=arr[0];
+	for(int i=0;i<n;i++){
+		if(arr[i]==temp_num){
+			temp_count++;
+		}
+		else{
+			if(temp_count>count){
+				count=temp_count;
+				num=temp_num;
+			}
+			temp_num=arr[i];
+			temp_count=1;
+		}
+	}
+	if(temp_count>count){
+		count=temp_count;
+		num=temp_num;
+	}
+	return num;
+}
+
+
+
 int factors(int num,int arr[]){
 	int prime=2,ref=0;
 	int count=0,index=0;
@@ -65,3 +94,4 @@ int factors(int num,int arr[]){
 	}
 	return count;
 }
+
