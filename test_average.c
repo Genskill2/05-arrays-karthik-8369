71d59d665b4c2539b68a3f8bad75780f528f4793
karthik-8369 <84717577+karthik-8369@users.noreply.github.com>
2021-06-18 21:53:56 +0530

    Update test_average.c

--- test_average.c
+++ test_average.c
@@ -12,9 +12,3 @@ int main(void) {
   assert (average(y, 1) == 5.0);
   printf("Average: passed\n");
   }
-float average(int avg[],int a){
-  int i,d=0;
-  for(i=0;i<a;i++){
-  d=d+avg[i];
-  }
-  return d/a;}
